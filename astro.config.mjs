b12312c97890539d87708a7860ef74b1cad17422
aasuss <shubhanshaasu@gmail.com>
2024-01-21 20:36:41 +0530

    GHP Deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), svelte()]
+  integrations: [tailwind(), sitemap(), svelte()],
+  site: 'https://aasuss.github.io',
+  base: '/ART-Website',
 });
