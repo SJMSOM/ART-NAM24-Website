22e8ffda699d020f4b4eba74ee518f71d2a4d4e1
aasuss <shubhanshaasu@gmail.com>
2024-01-24 18:28:44 +0530

    Thank you message on completion

---
+++ src/components/form/Completed.svelte
@@ -0,0 +1,8 @@
+<div class="mt-8 flex w-full lg:flex-row lg:items-start flex-col items-center">
+    <div class="bg-white/10 p-5 rounded-md">
+        <div class="flex flex-col w-full">
+            <div class="text-center p-2 mt-2 lg:text-2xl text-white font-display">Thank you for Registering!</div>
+            <div class="text-center p-2 mt-2 text-white">We eagerly await your return to campus.</div>
+        </div>
+    </div>
+</div>
